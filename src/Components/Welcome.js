2addcb9fc05c4016a2f81769520a657afada488a
radha-soni <er.radha17@gmail.com>
2019-11-02 20:57:23 +0530

    fix: state changes to unmounted component error  & changeState error

--- src/Components/Welcome.js
+++ src/Components/Welcome.js
@@ -1,115 +1,135 @@
-import React, { useState } from "react";
-import { Redirect } from "react-router-dom";
+import React, { useState } from 'react';
+import { Redirect } from 'react-router-dom';
 
 function Welcome(props) {
-  function handleLogout() {
-    props.changeState();
-  }
-  const [text, setText] = useState("add admin");
+	function handleLogout() {
+		props.changeState();
+	}
+	const [, setadmin] = useState(null);
+	let updateEmail;
+	function handleAdminChange(e) {
+		updateEmail = e.target.value;
+		fetch('http://localhost:8080/admin/add', {
+			method: 'POST',
+			body: JSON.stringify({
+				email: e.target.value
+			}),
+			headers: {
+				'Content-Type': 'application/json'
+			}
+		})
+			.then((res) => res.json())
+			.then((response) => {
+				handleAdmin();
+			})
+			.catch((error) => console.error('Error:', error));
+	}
+	function handleAdminRemove(e) {
+		updateEmail = e.target.value;
+		fetch('http://localhost:8080/admin/remove', {
+			method: 'POST',
+			body: JSON.stringify({
+				email: e.target.value
+			}),
+			headers: {
+				'Content-Type': 'application/json'
+			}
+		})
+			.then((res) => res.json())
+			.then((response) => {
+				handleAdmin();
+			})
+			.catch((error) => console.error('Error:', error));
+	}
+	function handleAdmin() {
+		if (props.users) {
+			for (let i = 0; i < props.users.length; i++) {
+				if (props.users[i].email === updateEmail) {
+					if (props.users[i].admin) {
+						setadmin(false);
+					} else {
+						setadmin(true);
+					}
+				}
+			}
+		}
+	}
 
-  function handleAdminChange(e) {
-    setText("remove admin");
-    fetch("http://localhost:8080/admin/add", {
-      method: "POST",
-      body: JSON.stringify({
-        email: e.target.value
-      }),
-      headers: {
-        "Content-Type": "application/json"
-      }
-    })
-      .then(res => res.json())
-      .then(response => {
-        console.log("Success:", console.log(response));
-      })
-      .catch(error => console.error("Error:", error));
-    console.log(props.users.email);
-  }
-  function handleAdminRemove(e) {
-    setText("add admin");
-    fetch("http://localhost:8080/admin/remove", {
-      method: "POST",
-      body: JSON.stringify({
-        email: e.target.value
-      }),
-      headers: {
-        "Content-Type": "application/json"
-      }
-    })
-      .then(res => res.json())
-      .then(response => {
-        console.log(response);
-        console.log("Success:", console.log(response));
-      })
-      .catch(error => console.error("Error:", error));
-  }
+	return (
+		<>
+			{props.loggedIn ? (
+				<div className='welcome-page'>
+					<h1 id='welcome-msg'>
+						Welcome to NOBROKER,{' '}
+						{props.user.firstName + ' ' + props.user.lastName}
+					</h1>
 
-  return (
-    <>
-      {props.loggedIn ? (
-        <div className="welcome-page">
-          <h1 id="welcome-msg">
-            Welcome to NOBROKER,{" "}
-            {props.user.firstName + " " + props.user.lastName}
-          </h1>
+					<div id='btn-container'>
+						<button
+							id='logout-btn'
+							type='submit'
+							onClick={handleLogout}
+						>
+							Logout
+						</button>
+					</div>
 
-          <div id="btn-container">
-            <button id="logout-btn" type="submit" onClick={handleLogout}>
-              Logout
-            </button>
-          </div>
+					{props.user.admin ? (
+						<div id='table-container'>
+							<table>
+								<tbody>
+									<tr id='header'>
+										<th>First name</th>
+										<th>Last name</th>
+										<th>Email</th>
+										<th>Add admin</th>
+									</tr>
 
-          {props.user.admin ? (
-            <div id="table-container">
-              <table>
-                <tbody>
-                  <tr id="header">
-                    <th>First name</th>
-                    <th>Last name</th>
-                    <th>Email</th>
-                    <th>Add admin</th>
-                  </tr>
+									{props.users.map((obj, index) => {
+										return (
+											<tr key={index}>
+												<td>{obj.firstName}</td>
+												<td>{obj.lastName}</td>
+												<td>{obj.email}</td>
 
-                  {props.users.map((obj, index) => {
-                    return (
-                      <tr key={index}>
-                        <td>{obj.firstName}</td>
-                        <td>{obj.lastName}</td>
-                        <td>{obj.email}</td>
-
-                        <td>
-                          {!obj.admin ? (
-                            <button
-                              id="make-admin-btn"
-                              value={obj.email}
-                              onClick={handleAdminChange}
-                            >
-                              add admin
-                            </button>
-                          ) : (
-                            <button
-                              id="make-admin-btn"
-                              value={obj.email}
-                              onClick={handleAdminChange}
-                            >
-                              remove admin
-                            </button>
-                          )}
-                        </td>
-                      </tr>
-                    );
-                  })}
-                </tbody>
-              </table>
-            </div>
-          ) : (
-            ""
-          )}
-        </div>
-      ) : (
-        <Redirect to="/login" />
-      )}
-    </>
-  );
+												<td>
+													{obj.admin ? (
+														<button
+															id='make-admin-btn'
+															value={obj.email}
+															onClick={
+																handleAdminRemove
+															}
+														>
+															remove
+														</button>
+													) : (
+														<button
+															id='make-admin-btn'
+															value={obj.email}
+															onClick={
+																handleAdminChange
+															}
+														>
+															add
+														</button>
+													)}
+												</td>
+											</tr>
+										);
+									})}
+								</tbody>
+							</table>
+						</div>
+					) : (
+						''
+					)}
+				</div>
+			) : (
+				<Redirect to='/login' />
+			)}
+		</>
+	);
 }
+
 export default Welcome;
--- src/index.js
+++ src/index.js
@@ -1,31 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-// import {
-//   Route,
-//   withRouter,
-//   Link,
-//   BrowserRouter as Router
-// } from "react-router-dom";
-import App from "./App";
-// import Login from "./Components/Login";
-// import Register from "./Components/Register";
-// import Welcome from "./Components/Welcome";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
 
-import * as serviceWorker from "./serviceWorker";
+import * as serviceWorker from './serviceWorker';
 
-// const routing = (
-//   <Router>
-//     <div>
-//       <Route exact path="/" component={Login} />
-//       <Route path="/login" component={Login} />
-//       <Route path="/register" component={Register} />
-//       <Route path="/Welcome" component={Welcome} />
-//     </div>
-//   </Router>
-// );
-
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
