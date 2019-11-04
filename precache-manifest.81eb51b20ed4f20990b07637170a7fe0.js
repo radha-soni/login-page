b2822b098113e640b9e7697bc68586f3a7a970f5
radha-soni <er.radha17@gmail.com>
2019-11-04 16:43:51 +0530

    Updates

---
+++ precache-manifest.81eb51b20ed4f20990b07637170a7fe0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d985e468003c53b682b98077788b880c",
+    "url": "/login-page/index.html"
+  },
+  {
+    "revision": "178f81193f0825ee5df2",
+    "url": "/login-page/static/css/main.7217ddc9.chunk.css"
+  },
+  {
+    "revision": "c2439b51188a2de78f72",
+    "url": "/login-page/static/js/2.0b384c05.chunk.js"
+  },
+  {
+    "revision": "178f81193f0825ee5df2",
+    "url": "/login-page/static/js/main.f8c61685.chunk.js"
+  },
+  {
+    "revision": "d17786305df33a65c342",
+    "url": "/login-page/static/js/runtime-main.df2b03ae.js"
+  }
+]);
